f356fe08d3e9e68d2f7452e3cf2780edb57cafee
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-20 17:17:15 +0000

    Header file for project added: included header guards and standard header files

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL
+#define SHELL
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <signal.h>
+
+
+#endif
