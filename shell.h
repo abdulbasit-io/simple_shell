e94c703ca94a0e99dee3f3a03105d7a1d138f75b
Abdulbasit <aabdulrafiu721@gmail.com>
2022-12-24 01:17:20 +0000

    Added builtins

--- shell.h
+++ shell.h
@@ -69,6 +69,10 @@ char *_which(char *str, list_t *env);
 int __exit(char **str, list_t *env, int num, char **command);
 void c_exit(char **str, list_t *env);
 int _execve(char **s, list_t *env, int num);
+void non_interactive(list_t *env);
+int prompt(char **en);
+int built_in(char **token, list_t *env, int num, char **command);
+char *ignore_space(char **str);
 
 
 /* Error messages functions*/
